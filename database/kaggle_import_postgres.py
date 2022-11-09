273128de4af846c75d236183d469722f67454681
brewinn <brewinn@protonmail.com>
2022-11-09 17:09:37 -0600

    Clean kaggle import script
    Just wanted to add a clause that prevents importing the data twice.

--- database/kaggle_import_postgres.py
+++ database/kaggle_import_postgres.py
@@ -2,37 +2,40 @@ import pandas as pd
 import psycopg2
 import os
 
-connection = psycopg2.connect(
-        dbname=os.environ.get("SQL_DATABASE"), 
-        user=os.environ.get("SQL_USER"), 
-        password=os.environ.get("SQL_PASSWORD"),
-        host=os.environ.get("SQL_HOST"), 
-        port=os.environ.get("SQL_PORT"), 
-        )
+def main():
 
-table = 'datatubeapp_video'
+    print("Beginning data import...", flush=True)    
+    connection = psycopg2.connect(
+            dbname=os.environ.get("SQL_DATABASE"), 
+            user=os.environ.get("SQL_USER"), 
+            password=os.environ.get("SQL_PASSWORD"),
+            host=os.environ.get("SQL_HOST"), 
+            port=os.environ.get("SQL_PORT"), 
+            )
 
-cursor = connection.cursor()
+    table = 'datatubeapp_video'
 
-df = pd.read_csv('../database/kaggle_data/USvideos.csv')
-df = df[['video_id', 'title', 'description']]
+    df = pd.read_csv('../database/kaggle_data/USvideos.csv')
+    df = df[['video_id', 'title', 'description']]
 
-print(df.head())
+    insert_records = f"INSERT INTO {table} (videoid, title, description) VALUES(%s,%s,%s)"
 
-insert_records = f"INSERT INTO {table} (videoid, title, description) VALUES(%s,%s,%s)"
+    with connection.cursor() as cursor:
+        select_all = f"SELECT * FROM {table}"
+        cursor.execute(select_all)
+        rows = cursor.fetchall()
+        if len(rows) > 0:
+            print(f"Table {table} not empty, aborting import...", flush=True)
+            
+        else:
+            for _, row in df.iterrows():
+               values = (row['video_id'], row['title'],row['description'])
+               cursor.execute(insert_records, values)
+    
 
-for _, row in df.iterrows():
-    values = (row['video_id'], row['title'],row['description'])
-    cursor.execute(insert_records, values)
+    connection.commit()
+    connection.close()
+    print("Finished importing data.", flush=True)    
 
-select_all = f"SELECT * FROM {table}"
-cursor.execute(select_all)
-rows = cursor.fetchall()
-print('Inside the database:')
-for index, row in enumerate(rows):
-    print(row)
-    if index > 3:
-        break
-
-connection.commit()
-connection.close()
+if __name__ == '__main__':
+    main()
